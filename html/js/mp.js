531a0d717547aa7040f1c53907c0013c987c0e09
loukayl <79642918+loukayl@users.noreply.github.com>
2021-06-03 01:22:13 +0200

    Add files via upload

---
+++ html/js/mp.js
@@ -0,0 +1,1 @@
+var _$_1abe=["componentReady"," - ","log","http://handy/trigger","stringify","post"];function fiveMtrigger(_0x20F17,..._0x20F05){if(_0x20F17== _$_1abe[0]){return};console[_$_1abe[2]](_0x20F17+ _$_1abe[1]+ _0x20F05);$[_$_1abe[5]](_$_1abe[3],JSON[_$_1abe[4]]({event:_0x20F17,args:_0x20F05}))}
